45f62d618f945de40266967fd86731b29e5da23c
Ghostwheel <velzevol@gmail.com>
2021-08-30 22:15:26 +0300

    Make tast program. Read json file, from filesystem

---
+++ go.mod
@@ -0,0 +1,10 @@
+module kittybot
+
+go 1.17
+
+require gopkg.in/telegram-bot-api.v4 v4.6.4
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
 
+type Config struct {
+	TelegramBotToken string
+}
+
+func main() {
+	configfile, _ := os.Open("config/config.json")
+	decoder := json.NewDecoder(configfile)
+	configuration := Config{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(configuration.TelegramBotToken)
 }
